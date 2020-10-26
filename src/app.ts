54adc9c3735f664d95b54df4a16ac1c94e4e338e
DenisEps <denis.epshteyn@gmail.com>
2020-10-26 09:22:47 +0300

    adds fields validation, validation interface and function

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+interface Validate {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+const validate = (validateInput: Validate) => {
+  let isValid = true;
+  if (validateInput.required) {
+    isValid = isValid && validateInput.value.toString().trim().length !== 0;
+  }
+  if (validateInput.minLength != null && typeof validateInput.value === 'string') {
+    isValid = isValid && validateInput.value.length >= validateInput.minLength; 
+  }
+  if (validateInput.maxLength != null && typeof validateInput.value === 'string') {
+    isValid = isValid && validateInput.value.length <= validateInput.maxLength; 
+  }
+  if (validateInput.min != null && typeof validateInput.value === 'number') {
+    isValid = isValid && validateInput.value >= validateInput.min;
+  }
+  if (validateInput.max != null && typeof validateInput.value === 'number') {
+    isValid = isValid && validateInput.value <= validateInput.max;
+  }
+  return isValid;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -22,9 +51,41 @@ class ProjectInput {
     this.attach();
   }
 
-  private submitHandler(e: Event) {
+  private fetchUserInput(): [string, string, number] | void {
+    const title = this.titleInputElement.value;
+    const desc = this.descriptionInputElement.value;
+    const people = this.peopleInputElement.value;
+
+    const titleValidate: Validate = {
+      value: title,
+      required: true,
+    }
+    const descValidate: Validate = {
+      value: desc,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidate: Validate = {
+      value: +people,
+      required: true,
+      min: 1,
+      max: 6,
+    }
+
+    if (!validate(titleValidate) || !validate(descValidate) || !validate(peopleValidate)) {
+      alert('Вы ввели неверные данные, попробуйте снова')
+    }
+
+    return [title, desc, +people];
+  }
+
+  private submitHandler(e: Event): void {
     e.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.fetchUserInput()
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+    }
   }
 
   private configure() {
